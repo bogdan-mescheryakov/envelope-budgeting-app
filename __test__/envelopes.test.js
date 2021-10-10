6249118cd65b11d6279661ae1322e6da6a8487ba
bogdan-mescheryakov <bogdan.mescheryakov@gmail.com>
2021-10-10 23:12:05 +0800

    improved test file readability

--- __test__/envelopes.test.js
+++ __test__/envelopes.test.js
@@ -4,8 +4,8 @@ let request = require('supertest');
 
 jest.setTimeout(30000);
 
-describe('GET/envelopes', () => {
-    it('respondes with an array of json', done => {
+describe('envelopes-routes', () => {
+    it('GET / envelopes - success', done => {
         request(app)
             .get('/envelopes')
             .set('Accept', 'application/json')
@@ -14,47 +14,41 @@ describe('GET/envelopes', () => {
             .then(response => {
                 const resObject = response.body[0];
                 expect(resObject).toEqual(
-                    expect.objectContaining(
-                        {currentMoney: expect.any(String),
-                            envelopeName: expect.any(String),
-                            iconUrl: expect.any(String),
-                            id: expect.any(Number),
-                            isEmpty: false,
-                            monthLimit: expect.any(String),
-                            partOfBalance: null
-                        }
-                    )
+                    expect.objectContaining({
+                        currentMoney: expect.any(String),
+                        envelopeName: expect.any(String),
+                        iconUrl: expect.any(String),
+                        id: expect.any(Number),
+                        isEmpty: false,
+                        monthLimit: expect.any(String),
+                        partOfBalance: null
+                        })
                 )
                 done();
             })
             .catch(err => done(err))
-        });
     });
-    describe('GET/envelopes/:id', () => {
-        /*afterAll(() => {
-            db.sequelize.close()
-        });*/
-        it('respondes with a json object', done => {
-            request(app)
-                .get('/envelopes/1')
-                .set('Accept', 'application/json')
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .then(response => {
-                    const resObject = response.body;
-                    console.log(resObject);
-                    expect(resObject).toEqual(
-                        {currentMoney: expect.any(String),
-                                envelopeName: expect.any(String),
-                                iconUrl: expect.any(String),
-                                id: expect.any(Number),
-                                isEmpty: false,
-                                monthLimit: expect.any(String),
-                                partOfBalance: null
-                            }
-                            )
-                    done();
-                })
-                .catch(err => done(err))
-            });
-    });
+    
+    it('GET / envelopes/:id - success', done => {
+        request(app)
+            .get('/envelopes/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(response => {
+                const resObject = response.body;
+                console.log(resObject);
+                expect(resObject).toEqual({
+                    currentMoney: expect.any(String),
+                    envelopeName: expect.any(String),
+                    iconUrl: expect.any(String),
+                    id: expect.any(Number),
+                    isEmpty: false,
+                    monthLimit: expect.any(String),
+                    partOfBalance: null
+                    })
+                done();
+            })
+            .catch(err => done(err))
+    });   
+});
